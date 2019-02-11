215feec51016de2f724d81d5d18193e14485ce11
Jens Hofschröer <github@nigjo.de>
2019-02-11 10:00:39 +0100

    - compressing converted files in the attic - recreate "_dokuwiki.changes"

--- reindex.cmd
+++ reindex.cmd
@@ -5,8 +5,17 @@ call settings.cmd
 
 pushd "%DOKU_ANIMALS_HOME%\%ANIMAL%"
 
+if not exist data\pages\ (
+  echo WARNUNG:
+  echo Es wurden kein "pages" Verzeichnis gefunden. Wurde die neue Version bereits
+  echo ins Zielverzeichnis kopiert?
+  pause
+  goto :eof
+)
+
 if not exist data\media\logo.png (
-  call :cleanup data\attic || goto :ende
+  echo working in %CD%
+  rem call :cleanup data\attic || goto :ende
   call :cleanup data\cache || goto :ende
   call :cleanup data\index || goto :ende
   call :cleanup data\locks || goto :ende
@@ -24,7 +33,26 @@ REM )
 popd 
 pushd "%DOKU_HOME%"
 
-php bin\indexer.php
+php bin\indexer.php || goto :ende
+
+popd
+pushd "%DOKU_ANIMALS_HOME%\%ANIMAL%"
+
+if exist data\meta\_dokuwiki.changes del data\meta\_dokuwiki.changes
+if exist data\meta\_dokuwiki.changes del data\meta\_dokuwiki.changes
+(
+  for /F "delims=*" %%D in ('dir /b/S/A:D data\meta\*.*') do (
+    if exist "%%D\*.changes" type "%%D\*.changes" 2>nul
+  )
+) > _dokuwiki_unsorted.changes
+sort _dokuwiki_unsorted.changes /O data\meta\_dokuwiki.changes
+del _dokuwiki_unsorted.changes
+
+echo --- compressing old files
+for /F "delims=*" %%T in ('dir data\attic\*.txt /s/b') do (
+  "c:\Program Files\7-Zip\7z.exe" a -bso0 "%%T.gz" "%%T" && del "%%T" || goto :ende
+)
+echo --- done
 
 :ende
 popd
